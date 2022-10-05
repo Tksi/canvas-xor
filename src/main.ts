2478d04f08ffeffc49cbd6be81bc149996e3d625
Tksi <Tksi@users.noreply.github.com>
2022-10-05 18:58:59 +0900

    [add] xor

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ const drawImage = async ({
 }): Promise<void> => {
   const img = new Image();
   img.src = imgPath;
+
   return new Promise((resolve) => {
     img.onload = () => {
       ctx.drawImage(img, 0, 0);
@@ -21,7 +22,7 @@ const ctxs = Array.from(
   document.querySelectorAll('canvas') as NodeListOf<HTMLCanvasElement>
 ).map((canvas) => canvas.getContext('2d')!);
 
-const size = 512;
+const size = (document.querySelector('canvas') as HTMLCanvasElement).width;
 
 await drawImage({
   ctx: ctxs[0],
@@ -33,12 +34,12 @@ await drawImage({
   imgPath: '/img2.jpg',
 });
 
-console.log(ctxs[0].getImageData(0, 0, size, size));
-// const img = ctxs[0].getImageData(0, 0, size, size);
-const img = new ImageData(
-  ctxs[0].getImageData(0, 0, size, size).data,
-  size,
-  size
-);
+const img0 = ctxs[0].getImageData(0, 0, size, size);
+const img1 = ctxs[1].getImageData(0, 0, size, size);
 
-ctxs[2].putImageData(img, 0, 0);
+for (let i = 0; i < img0.data.length; i++) {
+  if ((i + 1) % 4 == 0) continue;
+  img0.data[i] = img0.data[i] ^ img1.data[i + 1];
+}
+
+ctxs[2].putImageData(img0, 0, 0);
