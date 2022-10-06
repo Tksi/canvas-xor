0d80a247205b8340ab811c6590eaea5f88683f6b
Tksi <Tksi@users.noreply.github.com>
2022-10-06 18:12:50 +0900

    [add] noise xor image

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,30 @@
 import './style.css';
-import { drawNoise } from './lib';
+import { drawImage, drawNoise } from './lib';
 
-console.time('timer');
 const ctxs = Array.from(
   document.querySelectorAll('canvas') as NodeListOf<HTMLCanvasElement>
 ).map((canvas) => canvas.getContext('2d')!);
 
-drawNoise({ ctx: ctxs[0] });
-drawNoise({ ctx: ctxs[1] });
-
 const size = (document.querySelector('canvas') as HTMLCanvasElement).width;
+
+await drawImage({
+  ctx: ctxs[0],
+  imgPath: '/img1.png',
+});
+
+await drawNoise({
+  ctx: ctxs[1],
+});
+
 const img0 = ctxs[0].getImageData(0, 0, size, size);
 const img1 = ctxs[1].getImageData(0, 0, size, size);
 
+console.time('timer');
 for (let i = 0; i < img0.data.length; i++) {
   if ((i + 1) % 4 == 0) continue;
-
   const xor = img0.data[i] ^ img1.data[i + 1];
   img0.data[i] = xor;
 }
+console.timeEnd('timer');
 
 ctxs[2].putImageData(img0, 0, 0);
-
-console.timeEnd('timer');
